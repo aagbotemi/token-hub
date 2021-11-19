31d47bbada338aa361c1631d6d98dc75e0a889f2
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-19 10:15:18 +0100

    react router setup and folder structure for pages configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import ConnectWallet from './pages/connectWallet/ConnectWallet'
+import HomePage from './pages/homePage/HomePage'
+import About from './pages/about/About'
+import NotFound from './pages/404/404'
 
 function App() {
+  
+  const currentAccount = true;
   return (
-    <div className="App">
-      Token Hub
-    </div>
+    <>
+      {currentAccount ? (
+        <ConnectWallet />
+      ) : (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      )}
+    </>
   );
 }
 
---
+++ src/pages/connectWallet/ConnectWallet.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConnectWallet = () => {
+    return (
+        <div>
+            This is the connect wallet Page
+        </div>
+    )
+}
+
+export default ConnectWallet
