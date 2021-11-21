eb0141e5c9d60c8c50b78c59f12ff6dad4056291
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-11-21 16:44:11 +0100

    404 page implemented

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './404.css'
 
 const NotFound = () => {
     return (
-        <div>
-            Page Not Found
+        <div className='page-not-found'>
+            <h2>Oops! Page not found.</h2>
+            <h1>404</h1>
+            <p>We can't find the page you're looking for.</p>
+            <Link to="/" className='mt-1'>Go back home</Link>
         </div>
     )
 }
